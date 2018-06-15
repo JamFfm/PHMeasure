df0391afcf4014e160ab1221e2e737e5da0d7d06
JamFfm <Jan.Battermann@t-online.de>
2018-06-15 23:55:40 +0200

    add voltage and digit

--- __init__.py
+++ __init__.py
@@ -10,14 +10,22 @@ from modules.core.props import Property
 @cbpi.sensor
 class PHSensor(SensorActive):
 
-    #channel = Property.Number("Channel", configurable=True, default_value=0)
-    MCPchannel = (Property.Select("MCP3008 Channel", options=["0", "1", "2", "3", "4", "5", "6", "7"], description="Enter channel-number of MCP3008"))
-    
+    MCPchannel = Property.Select("MCP3008 Channel", options=["0", "1", "2", "3", "4", "5", "6", "7"], description="Enter channel-number of MCP3008")
+    sensorType = Property.Select("Data Type", options=["pH Value", "Voltage", "Digits"], description="Select which type of data to register for this sensor")
+    # Use Data Types Voltage and Digits (MCP3008) for calibration
+
     def get_unit(self):
         '''
         :return: Unit of the sensor as string. Should not be longer than 3 characters
         '''
-        return " pH"
+        if self.sensorType == "pH Value":
+            return " pH"
+        elif self.sensorType == "Voltage":
+            return " V"
+        elif self.sensorType == "Digits":
+            return " Bit"
+        else:
+            return "select Data Type"
 
     def stop(self):
         '''
@@ -48,7 +56,6 @@ class PHSensor(SensorActive):
             ch = (self.MCPchannel)
             cbpi.app.logger.info('PH Sensor channel     %s' % (ch))                     #debug
             
-            #value = mcp.read_adc(0)                                                    #change MCP3008 channel here
             value= mcp.read_adc(int(ch))                                                #change MCP3008 channel here via propertys of sensor
             cbpi.app.logger.info('PH Sensor value     %s' % (value))                    #debug or calibration
 
@@ -57,12 +64,21 @@ class PHSensor(SensorActive):
 
             phvalue = ("%.2f" % (7 + ((2.532 - voltage) / 0.1839)))
             cbpi.app.logger.info("PH Sensor phvalue %s%s" % ((phvalue),("0")))          #debug or calibration
-            
-            self.data_received(phvalue)
 
-            #mcp.close()
+            if self.sensorType == "pH Value":
+                reading = phvalue
+            elif self.sensorType == "Voltage":
+                reading = "%.3f" % (voltage)
+            elif self.sensorType == "Digits":
+                reading = value
+            else:
+                pass
             
-            self.api.socketio.sleep(2)
+            self.data_received(reading)
+
+            #mcp.close()                                                                 #did not use this one, in case the CBPi stopps after a while use this lise. I did mir need i            
+            
+            self.api.socketio.sleep(3)
             
 
 @cbpi.initalizer()
