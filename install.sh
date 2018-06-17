899ae2240fedca8e3290601e420584b3fb2aefc7
JamFfm <Jan.Battermann@t-online.de>
2018-06-17 20:13:59 +0200

    change path to install Adafruit MCP3008 Lib in to craftbeerpi3 folder instead of home folder

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install build-essential python-dev python-smbus git
-cd ~
+cd /home/pi/craftbeerpi3
 git clone https://github.com/adafruit/Adafruit_Python_MCP3008.git
 cd Adafruit_Python_MCP3008
 sudo python setup.py install
